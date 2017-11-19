a40e07fe8b83c39a4f6f00bc9707edc4039f8745
SudarshanBabu <33774300+SudarshanBabu@users.noreply.github.com>
2017-11-19 10:43:21 +0530

    First java sample console application
    First java sample console application with add / diff two numbers

---
+++ JavaConsole/src/SampleMath.java
@@ -0,0 +1,49 @@
+
+public class SampleMath {
+	public static void main(String[] args)
+	{
+		int n1,n2;
+		java.io.BufferedReader reader1 = new java.io.BufferedReader (new java.io.InputStreamReader (System.in));
+	    try
+	    {
+	    System.out.println("Enter first number :");
+		String str1 = reader1.readLine();
+		n1 = Integer.parseInt(str1);
+		System.out.println(n1);
+	    }
+	    catch(Exception ex)
+	    {
+	    	n1 = 0;
+	    	System.out.println("unable to read the number");
+	    }
+		try
+		{
+	    System.out.println("Enter second number :");
+	    String str2 = reader1.readLine();
+		n2 = Integer.parseInt(str2);
+		System.out.println(n2);
+		}
+		catch(Exception ex)
+	    {
+	    	n2 = 0;
+	    	System.out.println("unable to read the number");
+	    }
+			
+		System.out.println(add(n1,n2));
+		System.out.println(diff(n1,n2));
+		
+	}
+	public static int add(int num1, int num2)
+	{
+		System.out.println("The sum of two numbers is :");
+		return num1+num2;
+	}
+	
+	public static int diff(int num1, int num2)
+	{
+		System.out.println("The diff of two numbers is :");
+		
+		return num1-num2;
+	}
+
+}
