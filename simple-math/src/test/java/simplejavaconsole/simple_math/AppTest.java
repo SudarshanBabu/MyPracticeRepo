fd666a7ff450ced5ed11efe2b2ede58ab52ca654
SudarshanBabu <33774300+SudarshanBabu@users.noreply.github.com>
2017-11-24 14:24:41 +0530

    Adding unit tests
    Adding unit tests

--- simple-math/src/test/java/simplejavaconsole/simple_math/AppTest.java
+++ simple-math/src/test/java/simplejavaconsole/simple_math/AppTest.java
@@ -35,4 +35,35 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    public final void testAdd() {
+		int x  = 1 ; int y = 1;
+		System.out.println("Expected result is 2");
+		int result = App.add(x,y);
+		System.out.println("Actual result is : " + result);
+	    assertEquals(2, result);
+	}
+
+	public final void testDiff() {
+		int x  = 5 ; int y = 2;
+		System.out.println("Expected result is 3");
+		int result = App.diff(x,y);
+		System.out.println("Actual result is : " + result);
+	    assertEquals(3, result);
+
+	public final void testProd() {
+		int x  = 4 ; int y = 2;
+		System.out.println("Expected result is 8");
+		int result = App.prod(x,y);
+		System.out.println("Actual result is : " + result);
+	    assertEquals(8, result);
+	}
+
+	public final void testMod() {
+		int x  = 9 ; int y = 2;
+		System.out.println("Expected result is 1");
+		int result = App.mod(x,y);
+		System.out.println("Actual result is : " + result);
+	    assertEquals(1, result);
+	}
 }
